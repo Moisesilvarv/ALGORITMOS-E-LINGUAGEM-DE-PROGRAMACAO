2262db92acf35f7ad3cb7ad2808082cf9cf00560
Moises silva <110834179+Moisesilvarv@users.noreply.github.com>
2022-11-30 22:51:06 -0300

    Exercicios da Faculdade

---
+++ Atividade 4/Exercicio 3.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp56
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // NOME: MOISES DA SILVA PIMENTA 
+            // 315-01AM - ALGORITMOS E LINGUAGEM DE PROGRAMAÇÃO 
+            // ATIVIDADE 4: EXERCICIO 3!
+
+            int[] q = new int[10];
+
+            int maior = 0, pos_mai = 0;
+            for (int i = 1; i < 10; i++)
+            {
+                Console.WriteLine("Digite os valores: ");
+                q[i] = int.Parse(Console.ReadLine());
+                if (q[i] < 0)
+                {
+                    Console.WriteLine("Valor inesperado. Apenas numeros positivos!");
+                    Environment.Exit(0);
+                }
+                else if (q[i] > maior)
+                {
+                    maior = q[i];
+                    pos_mai = i;
+                }
+            }
+            Console.WriteLine("Valor do maior elemento: " + maior);
+            Console.WriteLine("Posição do maior elemento: " + pos_mai);
+        }
+    }
+}
---
+++ Atividade 4/Exercicio 6.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp56
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // NOME: MOISES DA SILVA PIMENTA 
+            // 315-01AM - ALGORITMOS E LINGUAGEM DE PROGRAMAÇÃO 
+            // ATIVIDADE 4: EXERCICIO 6!
+
+            int[] a = new int[10];
+
+            for (int i = 1; i < 10; i++)
+            {
+                Console.WriteLine("Digite os valores: ");
+                a[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 9; i >= 0; i--)
+            {
+                Console.WriteLine("Inverso: " + a[i]);
+            }
+
+
+        }
+    }
+}
